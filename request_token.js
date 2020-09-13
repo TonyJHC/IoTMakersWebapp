5767f99c1ed05c258d8b2cde685bda026f82e9c3
kwanulee <kwlee@hansung.ac.kr>
2020-09-13 21:38:26 +0900

    초기 커밋

---
+++ request_token.js
@@ -0,0 +1,27 @@
+	var token;  // access token을 저장하기 위한 변수
+
+	function requestTokenAwareApi(callback, args){
+		var appId = "hO0sJiKUMxD0lQay";     // 변경해야 함
+		var secret = "Xpe9uFQz4ywJK9TD";    // 변경해야 함
+		var uName = "kwanu70";              // 변경해야 함
+		var pw = "2Kwanwoo!";               // 변경해야 함
+
+		//App ID, Secret, 계정아이디, 계정 비밀번호를 기반으로 access token 획득
+		$.ajax('https://iotmakers.kt.com/oauth/token', {
+			method: 'POST',
+			xhrFields: { withCredentials: false },
+			headers: { 'Authorization': 'Basic ' + btoa(appId + ':' + secret)},
+			data: { grant_type: 'password',
+				username: uName,        
+				password: pw       
+			},
+			success: function(result) {
+	        	token = result.access_token;    // 성공시, access token 저장
+	        },
+	        error: function(xhr,status,error){
+	        	console.log(xhr);
+	        }
+	    });
+	}
+
+	requestTokenAwareApi(); // access token 획득위한 코드 수행
